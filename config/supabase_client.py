0f325e621fbe4affa175c1f0a96eb40263ceb248
Alessandra Lira <105083697+AlleLira@users.noreply.github.com>
2026-03-13 11:16:33 -0300

    Update supabase_client.py

--- config/supabase_client.py
+++ config/supabase_client.py
@@ -1,6 +1,1 @@
-from supabase import create_client
 
-SUPABASE_URL= "https://bpmxqctyfuraguynitaf.supabase.co"
-SUPABASE_KEY = "sb_publishable_hWxpl3r0_tcsh3ttggbzOA_cn9YcEs5"
-
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
