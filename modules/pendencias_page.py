3c8c6969a4888e5b1c804d6acf2ed5026d46bd99
Alessandra Lira <105083697+AlleLira@users.noreply.github.com>
2026-03-13 11:47:39 -0300

    Update pendencias_page.py

--- modules/pendencias_page.py
+++ modules/pendencias_page.py
@@ -262,6 +262,7 @@ def tela_pendencias():
             inserir_pendencia(dados)
 
             st.session_state["pendencia_salva"] = True
+            st.rerun()
             
 
     # =========================
@@ -294,4 +295,5 @@ def tela_pendencias():
         else:
 
             st.warning("Nenhuma pendência encontrada no período selecionado.")
+
 
